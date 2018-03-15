b0b6c37f8423042b89ad5842215204d3021126c9
feifeibear <fangjiarui123@gmail.com>
2018-03-15 22:45:38 +0100

    test piz data

--- models/daint/submit_replica_daint.sh
+++ models/daint/submit_replica_daint.sh
@@ -19,13 +19,13 @@ module load TensorFlow/1.4.1-CrayGNU-17.08-cuda-8.0-python3
 
 # load virtualenv
 export WORKON_HOME=$SCRATCH/fjr/Envs
-source $WORKON_HOME/tf-uber/bin/activate
+source $WORKON_HOME/horovod_0314/bin/activate
 UPDATE_METHOD=horovod
 
 mkdir -p srun_log
 export PYTHON=python3
-srun -N ${SLURM_JOB_NUM_NODES} -n ${SLURM_JOB_NUM_NODES} ${PYTHON} ../scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet50 \
---variable_update=${UPDATE_METHOD} > ./srun_log/${UPDATE_METHOD}_${SLURM_JOB_NUM_NODES}_nodes.log 
+srun -N ${SLURM_JOB_NUM_NODES} -n ${SLURM_JOB_NUM_NODES} ${PYTHON} ../../scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --num_gpus=1 --batch_size=160 --model=resnet50 \
+--variable_update=${UPDATE_METHOD} > ./srun_log/${UPDATE_METHOD}_${SLURM_JOB_NUM_NODES}_B64_nodes.log 
 #--variable_update=replicated --all_reduce_spec=pscpu
 
 # deactivate virtualenv
